982d5958846a7668975e084bd8c5ec45f95531b1
Offuru <andrei.rozmarin@gmail.com>
2024-07-25 11:56:50 +0300

    Added title to city event and added handling for invalid arguments

--- cityevents/city_event.py
+++ cityevents/city_event.py
@@ -8,15 +8,17 @@ class CityEvent(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     city = db.Column(db.String(64))
+    title = db.Column(db.String(128))
     date = db.Column(db.Date)
     location = db.Column(db.String(128))
     description = db.Column(db.Text)
     price = db.Column(db.Float)
     active = db.Column(db.Boolean, default=True)
 
-    def __init__(self, city, date, location, description, price):
+    def __init__(self, city='', date='1000-01-01', title='', location='', description='', price=0.0):
         self.city = city
         self.date = datetime.strptime(date, '%Y-%m-%d').date()
+        self.title = title
         self.location = location
         self.description = description
         self.price = float(price)
--- cityevents/city_events_api.py
+++ cityevents/city_events_api.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 from flask_restful import Resource
 from city_event import CityEvent
+import copy
 
 from city_events_database import db
 
@@ -8,6 +9,10 @@ from city_events_database import db
 class CityEvents(Resource):
     def get(self):
         city = request.args.get('city')
+
+        if not city:
+            return 'Invalid arguments', 400
+
         events = db.session.query(CityEvent).filter(CityEvent.active)
         events = events.filter(CityEvent.city.like(city)).all()
 
@@ -20,36 +25,56 @@ class CityEvents(Resource):
         return response
 
     def post(self):
-        payload = request.args
+        payload = request.form
+        event = CityEvent()
+
+        if not all(key in event.to_dict().keys() for key in payload.keys()):
+            return 'Invalid arguments', 400
+
         event = CityEvent(**payload)
+        response_copy = CityEvent(**payload)
 
         db.session.add(event)
         db.session.commit()
 
-        response = jsonify(event.to_dict())
+        response = jsonify(response_copy.to_dict())
         response.status_code = 201
 
         return response
 
     def put(self):
-        event_id = int(request.args.get('id'))
+        event_id = request.args.get('id')
+
+        if not event_id:
+            return 'Invalid arguments', 400
+
+        event_id = int(event_id)
+
         events = db.session.query(CityEvent).filter(CityEvent.active)
         event = events.filter(CityEvent.id == event_id).first()
 
         if not event:
             return 'No event was found', 404
 
-        any(setattr(event, key, request.args.get(key)) for key in request.args if getattr(event, key))
+        any(setattr(event, key, request.form.get(key)) for key in request.form if getattr(event, key))
+
+        response_copy = copy.deepcopy(event)
 
         db.session.commit()
 
-        response = jsonify(event.to_dict())
+        response = jsonify(response_copy.to_dict())
         response.status_code = 200
 
         return response
 
     def delete(self):
-        event_id = int(request.args.get('id'))
+        event_id = request.args.get('id')
+
+        if not event_id:
+            return 'Invalid arguments', 400
+
+        event_id = int(event_id)
+
         events = db.session.query(CityEvent).filter(CityEvent.active)
         event = events.filter(CityEvent.id == event_id).first()
 
